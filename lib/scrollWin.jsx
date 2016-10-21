92dcfef98382d398992d0b512b06edb19833a5c6
Marat Shagiev <m_js@bk.ru>
2016-10-21 13:30:57 +0300

    comment and reformat (acasionnaly) scrollWin.jsx

--- lib/scrollWin.jsx
+++ lib/scrollWin.jsx
@@ -1,12 +1,17 @@
+/**
+ * scrollabel alert from Peter Kernel's Beginning ScriptUI
+ * */
 function scrollWin (input) {
-  if (input instanceof Array)     input = input.join ("\r");
-  
-  var w                   = new Window ("dialog", 'Scrollable alert'),
-      list                = w.add ("edittext", undefined, input, {multiline: true, scrolling: true});
-  
+  if (input instanceof Array) {
+    input = input.join ("\r");
+  }
+
+  var w    = new Window ("dialog", 'Scrollable alert'),
+      list = w.add ("edittext", undefined, input, {multiline: true, scrolling: true});
+
   list.maximumSize.height = w.maximumSize.height - 100;
   list.minimumSize.width  = 600;
-  
+
   w.add ("button", undefined, "Close", {name: "ok"});
   w.show ();
 }
