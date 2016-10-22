7c9861a19a9f2f70c0074f52544c234b4f2a5d82
Marat Shagiev <m_js@bk.ru>
2016-10-23 01:34:41 +0300

    delEmptyLays.jsx rewrite comments and rename some variables

--- delEmptyLays.jsx
+++ delEmptyLays.jsx
@@ -1,48 +1,49 @@
 ﻿/**
- * ai.jsx MaratShagiev: m_js@bk.ru 17.03.2015
+ * ai.jsx MaratShagiev@m_js@bk.ru 17.03.2015
  */
-delEmptyLays ();
+delEmptyLays();
 
 /**
- * ф-ция удаления всех пустых слоёв/подслоёв
+ * recirsively remove empty layers and sublayers
  */
-function delEmptyLays () {
+function delEmptyLays() {
 
-  for ( var i = 0; i < activeDocument.layers.length; i++ ) {
-    var lay = activeDocument.layers[ i ];
-    if ( _hasSubs ( lay ) ) {
-      _delSubs ( lay );
+  for (var i = 0; i < activeDocument.layers.length; i++) {
+    var lay = activeDocument.layers[i];
+    if (_hasSubs(lay)) {
+      _delSubs(lay);
     }
-    if ( _isEmpty ( lay ) && activeDocument.layers.length > 1 ) { // попытка удалить единственный слой приведёт к его переименованию в <layer>
+    // NOTE: trying to remove the only existing layer will lead to its renaming to <layer>
+    if (_isEmpty(lay) && activeDocument.layers.length > 1) {
       lay.locked == true ? lay.locked = false : '';
       lay.visible == false ? lay.visible = true : '';
-      lay.remove ();
+      lay.remove();
       i--;
     }
   }
 
   /**
-   * рекурсивная ф-ция удаления подслоя
-   * @param {Object} lay - объект класса Layer
-   * @return {Object} lay - объект класса Layer
+   * recursively remove sublayer
+   * @param {Object} lay - object of Layer class
+   * @return {Object} lay - object of Layer class
    */
-  function _delSubs ( lay ) {
-    for ( var i = 0; i < lay.layers.length; i++ ) {
-      var thisSubLay = _getSubs ( lay )[ i ];
+  function _delSubs(lay) {
+    for (var i = 0; i < lay.layers.length; i++) {
+      var thisSubLay = _getSubs(lay)[i];
 
-      if ( _isEmpty ( thisSubLay ) ) {
+      if (_isEmpty(thisSubLay)) {
         thisSubLay.locked == true ? thisSubLay.locked = false : '';
         thisSubLay.visible == false ? thisSubLay.visible = true : '';
-        thisSubLay.remove ();
+        thisSubLay.remove();
         i--;
       }
 
-      if ( _hasSubs ( thisSubLay ) ) {
-        var parent = _delSubs ( thisSubLay );
-        if ( _isEmpty ( parent ) ) {
+      if (_hasSubs(thisSubLay)) {
+        var parent = _delSubs(thisSubLay);
+        if (_isEmpty(parent)) {
           thisSubLay.locked == true ? thisSubLay.locked = false : '';
           thisSubLay.visible == false ? thisSubLay.visible = true : '';
-          thisSubLay.remove ();
+          thisSubLay.remove();
           i--;
         }
       }
@@ -51,40 +52,39 @@ function delEmptyLays () {
   }
 
   /**
-   * содержит ли слой объекты класса Layer
-   * @param  {Object} lay - объект класса Layer
-   * @return {Boolean} true, если есть подслои
+   * @param  {Object} lay - object of Layer class
+   * @return {Boolean} true, if has sublayers
    */
-  function _hasSubs ( lay ) {
+  function _hasSubs(lay) {
     try {
       return (lay.layers.length > 0);
-    } catch ( e ) {
+    } catch (e) {
       return false;
     }
   }
 
   /**
-   * содержит ли слой объекты классов PageItem или Layer
-   * @param  {Object} lay - объект класса Layer
-   * @return {Boolean} true, если слой пуст
+   * if layer contained object of classes PageItem or Layer
+   * @param  {Object} lay - object of Layer class
+   * @return {Boolean} true, if layer is empty
    */
-  function _isEmpty ( lay ) {
+  function _isEmpty(lay) {
     try {
       return lay.pageItems.length == 0 && lay.layers.length == 0;
-    } catch ( e ) {
+    } catch (e) {
       return false;
     }
   }
 
   /**
-   * получить коллекцию layers данного layer
-   * @param  {Object} lay - объект класса Layer
-   * @return {Object/Boolean} - коллекция layers / false
+   * get sublayers collection from current layer
+   * @param  {Object} lay - object of Layer class
+   * @return {Object/Boolean} - collection of Layers or false
    */
-  function _getSubs ( lay ) {
+  function _getSubs(lay) {
     try {
       return lay.layers;
-    } catch ( e ) {
+    } catch (e) {
       return false;
     }
   }
