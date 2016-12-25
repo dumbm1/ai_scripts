cd1d4d80c062b7250c941364e212fd3d14744272
Marat Shagiev <m_js@bk.ru>
2016-12-26 02:03:53 +0300

    Edit function lib/randStr.jsx
     - add error correct
     - add default value of the random string length
     - add @{parameter} numb - requited random string length

--- lib/randStr.jsx
+++ lib/randStr.jsx
@@ -1,7 +1,24 @@
 /**
  * .jsx for Adobe apps. Marat Shagiev. Date: 04.10.2014, Time: 2:41
  */
+/**
+ * random string generator
+ *
+ * @param {Number} n - required sting length; 1 <= n <= 17; default value is 8
+ * @return {String} str - random n-characters string
+ * */
+function randStr(numb) {
+  var n = numb;
 
-function makeRandStr() {
-  return ("@" + (+new Date()) * Math.random() * 10000).slice(-7, -1);
+  if (isNaN(parseFloat(n)) || !isFinite(n)) n = 8;
+  if (!arguments.length) n = 8;
+  if (n > 17) n = 17;
+  if (n < 1) n = 1;
+
+  var str = ((+new Date()) * Math.random() *
+    1e7 + // shift (delete) decimal point
+    new Array(n).join('0') // for the case when random number is 0
+  ).slice(0, n);
+
+  return str;
 }
