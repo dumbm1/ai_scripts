6984a7d5ae04d1a28cc6c757a5b9eb004ac7d3b8
marat shagiev <m.shagiev@flexoprint.msk.ru>
2017-10-19 18:21:42 +0300

    new action save as white plate - save a copy

---
+++ jsx/saveAsWhite.js
@@ -0,0 +1,134 @@
+//@target illustrator-19
+
+;(function saveAsWhitePlate() {
+  var d              = activeDocument,
+      dName          = d.name,
+      dPath          = d.path;
+  var str            = (d.path + '/W_' + d.name),
+      str_compatible = encodeStrToAnsii(new File(str).fsName);
+
+
+  {
+    var actStr = "" +
+      "/version 3" +
+      "/name [ 8" + " 536574204e616d65" + "]" + "/isOpen 1" + "/actionCount 1" + "/action-1 {" + " /name [ 11" + " 416374696f6e204e616d65" + " ]" +
+      " /keyIndex 0" +
+      " /colorIndex 0" +
+      " /isOpen 1" +
+      " /eventCount 1" +
+      " /event-1 {" +
+      " /useRulersIn1stQuadrant 0" +
+      " /internalName (adobe_saveACopyAs)" +
+      " /localizedName [ 11" +
+      " 53617665204120436f7079" +
+      " ]" +
+      " /isOpen 0" +
+      " /isOn 1" +
+      " /hasDialog 1" +
+      " /showDialog 0" +
+      " /parameterCount 11" +
+      " /parameter-1 {" +
+      " /key 1668116594" +
+      " /showInPalette -1" +
+      " /type (boolean)" +
+      " /value 1" +
+      " }" +
+      " /parameter-2 {" +
+      " /key 1885627936" +
+      " /showInPalette -1" +
+      " /type (boolean)" +
+      " /value 1" +
+      " }" +
+      " /parameter-3 {" +
+      " /key 1668445298" +
+      " /showInPalette -1" +
+      " /type (integer)" +
+      " /value 17" +
+      " }" +
+      " /parameter-4 {" +
+      " /key 1702392878" +
+      " /showInPalette -1" +
+      " /type (integer)" +
+      " /value 1" +
+      " }" +
+      " /parameter-5 {" +
+      " /key 1768842092" +
+      " /showInPalette -1" +
+      " /type (integer)" +
+      " /value 0" +
+      " }" +
+      " /parameter-6 {" +
+      " /key 1918989423" +
+      " /showInPalette -1" +
+      " /type (real)" +
+      " /value 100.0" +
+      " }" +
+      " /parameter-7 {" +
+      " /key 1886545516" +
+      " /showInPalette -1" +
+      " /type (integer)" +
+      " /value 0" +
+      " }" +
+      " /parameter-8 {" +
+      " /key 1936548194" +
+      " /showInPalette -1" +
+      " /type (boolean)" +
+      " /value 0" +
+      " }" +
+      " /parameter-9 {" +
+      " /key 1851878757" +
+      " /showInPalette -1" +
+      " /type (ustring)" +
+      " /value [ " + str_compatible.length / 2 + "" + // string length
+      "               " + str_compatible +
+      " ]" +
+      " }" +
+      " /parameter-10 {" +
+      " /key 1718775156" +
+      " /showInPalette -1" +
+      " /type (ustring)" +
+      " /value [ 35" +
+      " 41646f626520496c6c7573747261746f7220416e7920466f726d617420577269" +
+      " 746572" +
+      " ]" +
+      " }" +
+      " /parameter-11 {" +
+      " /key 1702392942" +
+      " /showInPalette -1" +
+      " /type (ustring)" +
+      " /value [ 6" +
+      " 61692c616974" +
+      " ]" +
+      " }" +
+      " }" +
+      "}"
+  }
+
+  runAction(actStr, "Action Name", "Set Name");
+
+  function runAction(aiActionString, aiActionName, aiActionSetName) {
+    var f = new File('~/ScriptAction.aia');
+    f.open('w');
+    f.write(aiActionString);
+    f.close();
+    app.loadAction(f);
+    f.remove();
+
+    app.doScript(aiActionName, aiActionSetName, false); // action name, set name
+    app.unloadAction(aiActionSetName, ""); // set name
+  }
+
+  function encodeStrToAnsii(str) {
+    var result = '';
+    for (var i = 0; i < str.length; i++) {
+      var chr = File.encode(str[i]);
+      chr     = chr.replace(/%/gmi, '');
+      if (chr.length == 1) {
+        result += chr.charCodeAt(0).toString(16);
+      } else {
+        result += chr.toLowerCase();
+      }
+    }
+    return result;
+  }
+}());
