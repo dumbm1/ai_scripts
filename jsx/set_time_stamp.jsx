6a0f24d9a14fdaba1111918e4ea5217cc72e02b4
Marat Shagiev <m_js@bk.ru>
2018-02-08 17:42:58 +0300

    Add jsx set_time_stamp v1.0 that modify axisting text frame with name of '__current_date_and_time__' or rewrite and rename the first selected text frame

---
+++ jsx/set_time_stamp.jsx
@@ -0,0 +1,59 @@
+//@target illustrator
+set_time_stamp();
+function set_time_stamp() {
+  var txtFrameDateName = '__current_date_and_time__',
+      txtFrameDate,
+      now              = new Date(),
+      strDate          = 'Дата:\n' + _formatDate(now) + '\nВремя:\n' + _formatTime(now);
+
+  try {
+    txtFrameDate = activeDocument.textFrames.getByName(txtFrameDateName);
+    txtFrameDate.contents = strDate;
+  } catch (e) {
+    var txtFrameDate = _getSelFrame();
+    if (!txtFrameDate) return;
+    if (!(+confirm('Make new date frame from selected frame?'))) return;
+    txtFrameDate.contents = strDate;
+    txtFrameDate.name = txtFrameDateName;
+    return;
+  }
+
+  function _getSelFrame() {
+    var selFrame = false;
+    if (!selection.length) return;
+    if (selection.typename == 'TextRange') return;
+    if (!selection[0]) return;
+
+    for (var i = 0; i < selection.length; i++) {
+      if (selection[i].typename != 'TextFrame') continue;
+      selFrame = selection[i];
+      break;
+    }
+    return selFrame;
+  }
+
+  function _formatTime(date) {
+
+    var hh = date.getHours();
+    if (hh < 10) hh = '0' + hh;
+
+    var mn = date.getMinutes();
+    if (mn < 10) mn = '0' + mn;
+
+    return hh + ':' + mn;
+  }
+
+  function _formatDate(date) {
+
+    var dd = date.getDate();
+    if (dd < 10) dd = '0' + dd;
+
+    var mm = date.getMonth() + 1;
+    if (mm < 10) mm = '0' + mm;
+
+    var yy = date.getFullYear() % 100;
+    if (yy < 10) yy = '0' + yy;
+
+    return dd + '.' + mm + '.' + yy;
+  }
+}
