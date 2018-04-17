d57bb11b07be641e7971106bb54f327ccb42e2da
Marat Shagiev <m_js@bk.ru>
2018-04-17 14:38:17 +0300

    Fix bug with Photoshop open

--- jsx/editLinks.jsx
+++ jsx/editLinks.jsx
@@ -8,7 +8,7 @@
 
 //@target illustrator
 
-(function editLinks () {
+(function editLinks() {
   var links = activeDocument.placedItems,
       paths = {};
 
@@ -21,20 +21,20 @@
   for (var key in paths) {
     var fileExt;
 
-    if (~key.lastIndexOf ('.')) {
-      fileExt = key.slice (key.lastIndexOf ('.'));
+    if (~key.lastIndexOf('.')) {
+      fileExt = key.slice(key.lastIndexOf('.'));
     } else {
       continue;
     }
 
-    switch (fileExt.toLowerCase ()) {
+    switch (fileExt.toLowerCase()) {
       case '.ai':
       case '.pdf':
       case '.svg':
-        _openInIllustrator (key);
+        _openInIllustrator(key);
         break;
       case '.eps':
-        _processEps (key);
+        _processEps(key);
         break;
       case '.psd':
       case '.png':
@@ -45,7 +45,7 @@
       case '.tiff':
       case '.tif':
       case '.bmp':
-        _openInPhotoshop (key);
+        _openInPhotoshop(key);
         break;
       default:
         break;
@@ -55,30 +55,115 @@
   /**
    * LIB
    * */
-  function _openInPhotoshop (filePath) {
-    var bt    = new BridgeTalk ();
+  function _openInPhotoshop(filePath) {
+    if (!new File(filePath).exists) return false;
+
+    startPhotoshopExeBat(4000, 3);
+
+    var bt = new BridgeTalk();
+
     bt.target = 'photoshop';
-    bt.body   = 'var f = (new File("' + filePath + '")); app.open(f);';
-    bt.send ();
-  }
+    bt.body = 'var f = (new File("' + filePath + '")); app.open(f);';
 
-  function _openInIllustrator (filePath) {
-    open (new File (filePath));
-  }
+    /*bt.onError = function (errorMsgObject) {
+     $.writeln('error:\n' + showObjDeep(errorMsgObject));
+     };
+     bt.onTimeout = function (timeoutMsgObject) {
+     $.writeln('timeout:\n' + showObjDeep(timeoutMsgObject));
+     };
+     bt.onReceived = function (origMsgObject) {
+     $.writeln('received:\n' + showObjDeep(origMsgObject));
+     };
+     bt.onResult = function (responseMsgObject) {
+     $.writeln('result:\n' + showObjDeep(responseMsgObject));
+     };*/
 
-  function _processEps (filePath) {
-    var epsFile = new File (filePath),
-        epsFileStr;
+    bt.send();
 
-    epsFile.open ('r');
-    epsFileStr = epsFile.read ();
-
-    if (epsFileStr.match ('Creator: Adobe Photoshop')) {
-      _openInPhotoshop (filePath);
-    } else {
-      _openInIllustrator (filePath);
+    function startPhotoshopExeBat(timeout, numAttempts) {
+      for (var i = 0; i < numAttempts; i++) {
+        switch (BridgeTalk.getStatus('photoshop')) {
+          case 'BUSY':
+          case 'IDLE':
+          case 'PUMPING':
+          case 'UNDEFINED':
+            break;
+          case 'ISNOTRUNNING':
+            _execFile(Folder.temp.absoluteURI + '/' + 'startPs.bat', 'start photoshop.exe');
+            $.sleep(timeout);
+            break;
+          case 'ISNOTINSTALLED':
+            throw new Error('Photoshop is not installed');
+            break;
+          default:
+            break;
+        }
+      }
     }
 
-    epsFile.close ();
+    /*function showObjDeep(obj) {
+     var str = '{\n';
+     var indent = 1;
+
+     showObj(obj);
+
+     function showObj(obj) {
+
+     for (var key in obj) {
+     if (typeof obj[key] == 'object' /!*&& !obj[key].splice*!/) {
+     str += addIndent(indent) + key + ':\n';
+     ++indent;
+     showObj(obj[key]);
+     } else {
+     str += addIndent(indent) + key + ': ' + obj[key] + ' [' + typeof obj[key] + '],\n';
+     }
+     }
+     indent--;
+     }
+
+     return str + '}';
+     function addIndent(i) {
+     return new Array(i).join('_');
+     }
+     }*/
+
+    /**
+     * make new file by full path, write to disk with some file contenr, execute file
+     *
+     * @param {String} filePath - FULL path (include file-extension)
+     * @param {String} fileContent - content to new file
+     */
+    function _execFile(filePath, fileContent) {
+      if (new File(filePath).exists) {
+        new File(filePath).remove();
+      }
+
+      var f = new File(filePath);
+
+      f.open('e');
+      f.write(fileContent);
+      f.close();
+      f.execute();
+    }
   }
-} () );
+
+  function _openInIllustrator(filePath) {
+    open(new File(filePath));
+  }
+
+  function _processEps(filePath) {
+    var epsFile = new File(filePath),
+        epsFileStr;
+
+    epsFile.open('r');
+    epsFileStr = epsFile.read();
+
+    if (epsFileStr.match('Creator: Adobe Photoshop')) {
+      _openInPhotoshop(filePath);
+    } else {
+      _openInIllustrator(filePath);
+    }
+
+    epsFile.close();
+  }
+}() );
