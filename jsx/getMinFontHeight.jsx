d121e9e41244b49d597cec2768c3390250972a80
Marat Shagiev <m_js@bk.ru>
2019-03-18 18:40:01 +0300

    fix bugs and resolve todo things in getMinFontHeight.jsx

--- jsx/getMinFontHeight.jsx
+++ jsx/getMinFontHeight.jsx
@@ -1,22 +1,21 @@
 ;(function getMinFontHeight() {
-  if (!selection.length) throw new Error ('NoSelection');
-  if (selection.length > 1) throw new Error('ManySelection');
-  if (selection[0].typename !== 'TextFrame') throw new Error ('SelectionIsNotATextFrame');
+  var PT_TO_MM = 0.352777778,
+      txtFrameDouble,
+      frameCurves,
+      res;
+  try {
+    txtFrameDouble = selection[0].duplicate();
+  } catch (e) {
+    txtFrameDouble = selection.parent.textFrames[0].duplicate();
+  }
 
+  txtFrameDouble.contents = 'w';
+  frameCurves = txtFrameDouble.createOutline();
+  res = [
+    Math.round(frameCurves.height * PT_TO_MM * 100) / 100,
+    Math.round(frameCurves.width * PT_TO_MM * 100) / 100
+  ];
+  frameCurves.remove();
 
-  var PT_TO_MM = 0.352777778;
-
-  var fr = selection[0];
-  var fr2 = fr.duplicate();
-  var result_h, result_w;
-  fr.selected = false;
-  fr2.contents = 'w';
-  fr2.createOutline();
-  fr2 = selection[0];
-  result_h = Math.round(fr2.height * PT_TO_MM * 1000) / 1000;
-  result_w = Math.round(fr2.width * PT_TO_MM * 1000) / 1000;
-  alert('height: ' + result_h + ' мм\n' + 'width: ' + result_w + ' мм');
-  fr2.remove();
-  fr.selected = true;
-// todo: this code work with point text only....
+  alert('h: ' + res[0] + ' mm\nw: ' + res[1] + ' mm');
 })();
