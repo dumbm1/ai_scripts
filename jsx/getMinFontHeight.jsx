ddc847382de872b4a9e4740dfb1ef00d2aa0b206
Marat Shagiev <m_js@bk.ru>
2019-03-27 14:21:04 +0300

    Add measure and output the capital letters

--- jsx/getMinFontHeight.jsx
+++ jsx/getMinFontHeight.jsx
@@ -3,22 +3,35 @@
 ;(function getMinFontHeight() {
   var PT_TO_MM = 0.352777778,
       txtFrameDouble,
+      txtFrameDoubleCaps,
       frameCurves,
-      fontH, fontW, res;
+      frameCurvesCaps,
+      fontH, fontW, fontHCaps, fontWCaps;
 
   try {
     txtFrameDouble = selection[0].duplicate();
+    txtFrameDoubleCaps = selection[0].duplicate();
+    _processDoubles();
   } catch (e) {
     txtFrameDouble = selection.parent.textFrames[0].duplicate();
+    txtFrameDoubleCaps = selection.parent.textFrames[0].duplicate();
+    _processDoubles();
+  } finally {
+    alert('Capital:\t' + Math.min(fontHCaps, fontWCaps) + ' mm\nSmall:\t' + Math.min(fontH, fontW) + ' mm');
   }
 
-  txtFrameDouble.contents = 'www';
-  frameCurves = txtFrameDouble.createOutline();
+  function _processDoubles() {
+    txtFrameDouble.contents = 'www';
+    txtFrameDoubleCaps.contents = 'WWW';
+    frameCurves = txtFrameDouble.createOutline();
+    frameCurvesCaps = txtFrameDoubleCaps.createOutline();
 
-  fontH = Math.round(frameCurves.height * PT_TO_MM * 100) / 100;
-  fontW = Math.round(frameCurves.width * PT_TO_MM * 100) / 100;
+    fontH = Math.round(frameCurves.height * PT_TO_MM * 100) / 100;
+    fontHCaps = Math.round(frameCurvesCaps.height * PT_TO_MM * 100) / 100;
+    fontW = Math.round(frameCurves.width * PT_TO_MM * 100) / 100;
+    fontWCaps = Math.round(frameCurvesCaps.width * PT_TO_MM * 100) / 100;
 
-  frameCurves.remove();
-
-  alert(Math.min(fontH, fontW));
+    frameCurves.remove();
+    frameCurvesCaps.remove();
+  }
 })();
