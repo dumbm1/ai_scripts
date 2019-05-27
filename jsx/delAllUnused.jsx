74d96299492da669782bdf5864468f603b66191e
Marat Shagiev <m_js@bk.ru>
2019-05-27 15:37:47 +0300

    delAllUnused: add name _safe_ to exception

--- jsx/delAllUnused.jsx
+++ jsx/delAllUnused.jsx
@@ -17,12 +17,12 @@
 
   function addCmykw() {
     var cmyko = {
-      "White":        [0, 0, 0, 0],
-      "Black":        [0, 0, 0, 100],
-      "CMYK Cyan":    [100, 0, 0, 0],
-      "CMYK Magenta": [0, 100, 0, 0],
-      "CMYK Yellow":  [0, 0, 100, 0]
-    }
+      'White'       : [0, 0, 0, 0],
+      'Black'       : [0, 0, 0, 100],
+      'CMYK Cyan'   : [100, 0, 0, 0],
+      'CMYK Magenta': [0, 100, 0, 0],
+      'CMYK Yellow' : [0, 0, 100, 0]
+    };
     for (var key in cmyko) {
       try {
         activeDocument.swatches.getByName(key);
@@ -48,13 +48,13 @@
       } catch (e) {
         if (e.message == 'No such element') {
           var sw = d.swatches.add();
-          sw.name       = swName;
-          var swCol     = new CMYKColor();
-          swCol.cyan    = values[0];
+          sw.name = swName;
+          var swCol = new CMYKColor();
+          swCol.cyan = values[0];
           swCol.magenta = values[1];
-          swCol.yellow  = values[2];
-          swCol.black   = values[3];
-          sw.color      = swCol;
+          swCol.yellow = values[2];
+          swCol.black = values[3];
+          sw.color = swCol;
           return sw;
         } else {
           alert(e);
@@ -81,6 +81,7 @@
        if (sw.name == 'Black') continue;
        }*/
 
+      if (sw.name.match(/^_safe_/)) continue;
       if (sw.color.typename == 'GradientColor') continue;
       if (sw.color.typename == 'PatternColor') continue;
 
@@ -100,7 +101,7 @@
     app.loadAction(f);
     f.remove();
     app.doScript(actName, setName, false); // action name, set name
-    app.unloadAction(setName, ""); // set name
+    app.unloadAction(setName, ''); // set name
   }
 
   /**
@@ -109,12 +110,12 @@
   function ungrSw() {
     if (activeDocument.swatchGroups.length < 1) return;
 
-    var d        = activeDocument;
-    var swGrps   = d.swatchGroups;
+    var d = activeDocument;
+    var swGrps = d.swatchGroups;
     var mainSwGr = d.swatchGroups[0];
 
     for (var i = 1; i < swGrps.length; i++) { // move swatches to base group
-      var swGr    = swGrps[i];
+      var swGr = swGrps[i];
       var swGrSws = swGr.getAllSwatches();
       for (var j = 0; j < swGrSws.length; j++) {
         var sw = swGrSws[j];
@@ -130,184 +131,184 @@
 
   function getStrForRmUnusedAct() {
     return '/version 3' +
-      '/name [ 12' +
-      '	64656c416c6c556e75736564' +
-      ']' +
-      '/isOpen 0' +
-      '/actionCount 1' +
-      '/action-1 {' +
-      '	/name [ 12' +
-      '		64656c416c6c556e75736564' +
-      '	]' +
-      '	/keyIndex 0' +
-      '	/colorIndex 2' +
-      '	/isOpen 0' +
-      '	/eventCount 8' +
-      '	/event-1 {' +
-      '		/useRulersIn1stQuadrant 0' +
-      '		/internalName (ai_plugin_swatches)' +
-      '		/localizedName [ 8' +
-      '			5377617463686573' +
-      '		]' +
-      '		/isOpen 0' +
-      '		/isOn 1' +
-      '		/hasDialog 0' +
-      '		/parameterCount 1' +
-      '		/parameter-1 {' +
-      '			/key 1835363957' +
-      '			/showInPalette -1' +
-      '			/type (enumerated)' +
-      '			/name [ 17' +
-      '				53656c65637420416c6c20556e75736564' +
-      '			]' +
-      '			/value 11' +
-      '		}' +
-      '	}' +
-      '	/event-2 {' +
-      '		/useRulersIn1stQuadrant 0' +
-      '		/internalName (ai_plugin_swatches)' +
-      '		/localizedName [ 8' +
-      '			5377617463686573' +
-      '		]' +
-      '		/isOpen 0' +
-      '		/isOn 1' +
-      '		/hasDialog 1' +
-      '		/showDialog 0' +
-      '		/parameterCount 1' +
-      '		/parameter-1 {' +
-      '			/key 1835363957' +
-      '			/showInPalette -1' +
-      '			/type (enumerated)' +
-      '			/name [ 13' +
-      '				44656c65746520537761746368' +
-      '			]' +
-      '			/value 3' +
-      '		}' +
-      '	}' +
-      '	/event-3 {' +
-      '		/useRulersIn1stQuadrant 0' +
-      '		/internalName (ai_plugin_brush)' +
-      '		/localizedName [ 5' +
-      '			4272757368' +
-      '		]' +
-      '		/isOpen 0' +
-      '		/isOn 1' +
-      '		/hasDialog 0' +
-      '		/parameterCount 1' +
-      '		/parameter-1 {' +
-      '			/key 1835363957' +
-      '			/showInPalette -1' +
-      '			/type (enumerated)' +
-      '			/name [ 17' +
-      '				53656c65637420416c6c20556e75736564' +
-      '			]' +
-      '			/value 8' +
-      '		}' +
-      '	}' +
-      '	/event-4 {' +
-      '		/useRulersIn1stQuadrant 0' +
-      '		/internalName (ai_plugin_brush)' +
-      '		/localizedName [ 5' +
-      '			4272757368' +
-      '		]' +
-      '		/isOpen 0' +
-      '		/isOn 1' +
-      '		/hasDialog 1' +
-      '		/showDialog 0' +
-      '		/parameterCount 1' +
-      '		/parameter-1 {' +
-      '			/key 1835363957' +
-      '			/showInPalette -1' +
-      '			/type (enumerated)' +
-      '			/name [ 12' +
-      '				44656c657465204272757368' +
-      '			]' +
-      '			/value 3' +
-      '		}' +
-      '	}' +
-      '	/event-5 {' +
-      '		/useRulersIn1stQuadrant 0' +
-      '		/internalName (ai_plugin_styles)' +
-      '		/localizedName [ 14' +
-      '			47726170686963205374796c6573' +
-      '		]' +
-      '		/isOpen 0' +
-      '		/isOn 1' +
-      '		/hasDialog 0' +
-      '		/parameterCount 1' +
-      '		/parameter-1 {' +
-      '			/key 1835363957' +
-      '			/showInPalette -1' +
-      '			/type (enumerated)' +
-      '			/name [ 17' +
-      '				53656c65637420416c6c20556e75736564' +
-      '			]' +
-      '			/value 14' +
-      '		}' +
-      '	}' +
-      '	/event-6 {' +
-      '		/useRulersIn1stQuadrant 0' +
-      '		/internalName (ai_plugin_styles)' +
-      '		/localizedName [ 14' +
-      '			47726170686963205374796c6573' +
-      '		]' +
-      '		/isOpen 0' +
-      '		/isOn 1' +
-      '		/hasDialog 1' +
-      '		/showDialog 0' +
-      '		/parameterCount 1' +
-      '		/parameter-1 {' +
-      '			/key 1835363957' +
-      '			/showInPalette -1' +
-      '			/type (enumerated)' +
-      '			/name [ 20' +
-      '				44656c6574652047726170686963205374796c65' +
-      '			]' +
-      '			/value 3' +
-      '		}' +
-      '	}' +
-      '	/event-7 {' +
-      '		/useRulersIn1stQuadrant 0' +
-      '		/internalName (ai_plugin_symbol_palette)' +
-      '		/localizedName [ 7' +
-      '			53796d626f6c73' +
-      '		]' +
-      '		/isOpen 0' +
-      '		/isOn 1' +
-      '		/hasDialog 0' +
-      '		/parameterCount 1' +
-      '		/parameter-1 {' +
-      '			/key 1835363957' +
-      '			/showInPalette -1' +
-      '			/type (enumerated)' +
-      '			/name [ 17' +
-      '				53656c65637420416c6c20556e75736564' +
-      '			]' +
-      '			/value 12' +
-      '		}' +
-      '	}' +
-      '	/event-8 {' +
-      '		/useRulersIn1stQuadrant 0' +
-      '		/internalName (ai_plugin_symbol_palette)' +
-      '		/localizedName [ 7' +
-      '			53796d626f6c73' +
-      '		]' +
-      '		/isOpen 0' +
-      '		/isOn 1' +
-      '		/hasDialog 1' +
-      '		/showDialog 0' +
-      '		/parameterCount 1' +
-      '		/parameter-1 {' +
-      '			/key 1835363957' +
-      '			/showInPalette -1' +
-      '			/type (enumerated)' +
-      '			/name [ 13' +
-      '				44656c6574652053796d626f6c' +
-      '			]' +
-      '			/value 5' +
-      '		}' +
-      '	}' +
-      '}'
+           '/name [ 12' +
+           '	64656c416c6c556e75736564' +
+           ']' +
+           '/isOpen 0' +
+           '/actionCount 1' +
+           '/action-1 {' +
+           '	/name [ 12' +
+           '		64656c416c6c556e75736564' +
+           '	]' +
+           '	/keyIndex 0' +
+           '	/colorIndex 2' +
+           '	/isOpen 0' +
+           '	/eventCount 8' +
+           '	/event-1 {' +
+           '		/useRulersIn1stQuadrant 0' +
+           '		/internalName (ai_plugin_swatches)' +
+           '		/localizedName [ 8' +
+           '			5377617463686573' +
+           '		]' +
+           '		/isOpen 0' +
+           '		/isOn 1' +
+           '		/hasDialog 0' +
+           '		/parameterCount 1' +
+           '		/parameter-1 {' +
+           '			/key 1835363957' +
+           '			/showInPalette -1' +
+           '			/type (enumerated)' +
+           '			/name [ 17' +
+           '				53656c65637420416c6c20556e75736564' +
+           '			]' +
+           '			/value 11' +
+           '		}' +
+           '	}' +
+           '	/event-2 {' +
+           '		/useRulersIn1stQuadrant 0' +
+           '		/internalName (ai_plugin_swatches)' +
+           '		/localizedName [ 8' +
+           '			5377617463686573' +
+           '		]' +
+           '		/isOpen 0' +
+           '		/isOn 1' +
+           '		/hasDialog 1' +
+           '		/showDialog 0' +
+           '		/parameterCount 1' +
+           '		/parameter-1 {' +
+           '			/key 1835363957' +
+           '			/showInPalette -1' +
+           '			/type (enumerated)' +
+           '			/name [ 13' +
+           '				44656c65746520537761746368' +
+           '			]' +
+           '			/value 3' +
+           '		}' +
+           '	}' +
+           '	/event-3 {' +
+           '		/useRulersIn1stQuadrant 0' +
+           '		/internalName (ai_plugin_brush)' +
+           '		/localizedName [ 5' +
+           '			4272757368' +
+           '		]' +
+           '		/isOpen 0' +
+           '		/isOn 1' +
+           '		/hasDialog 0' +
+           '		/parameterCount 1' +
+           '		/parameter-1 {' +
+           '			/key 1835363957' +
+           '			/showInPalette -1' +
+           '			/type (enumerated)' +
+           '			/name [ 17' +
+           '				53656c65637420416c6c20556e75736564' +
+           '			]' +
+           '			/value 8' +
+           '		}' +
+           '	}' +
+           '	/event-4 {' +
+           '		/useRulersIn1stQuadrant 0' +
+           '		/internalName (ai_plugin_brush)' +
+           '		/localizedName [ 5' +
+           '			4272757368' +
+           '		]' +
+           '		/isOpen 0' +
+           '		/isOn 1' +
+           '		/hasDialog 1' +
+           '		/showDialog 0' +
+           '		/parameterCount 1' +
+           '		/parameter-1 {' +
+           '			/key 1835363957' +
+           '			/showInPalette -1' +
+           '			/type (enumerated)' +
+           '			/name [ 12' +
+           '				44656c657465204272757368' +
+           '			]' +
+           '			/value 3' +
+           '		}' +
+           '	}' +
+           '	/event-5 {' +
+           '		/useRulersIn1stQuadrant 0' +
+           '		/internalName (ai_plugin_styles)' +
+           '		/localizedName [ 14' +
+           '			47726170686963205374796c6573' +
+           '		]' +
+           '		/isOpen 0' +
+           '		/isOn 1' +
+           '		/hasDialog 0' +
+           '		/parameterCount 1' +
+           '		/parameter-1 {' +
+           '			/key 1835363957' +
+           '			/showInPalette -1' +
+           '			/type (enumerated)' +
+           '			/name [ 17' +
+           '				53656c65637420416c6c20556e75736564' +
+           '			]' +
+           '			/value 14' +
+           '		}' +
+           '	}' +
+           '	/event-6 {' +
+           '		/useRulersIn1stQuadrant 0' +
+           '		/internalName (ai_plugin_styles)' +
+           '		/localizedName [ 14' +
+           '			47726170686963205374796c6573' +
+           '		]' +
+           '		/isOpen 0' +
+           '		/isOn 1' +
+           '		/hasDialog 1' +
+           '		/showDialog 0' +
+           '		/parameterCount 1' +
+           '		/parameter-1 {' +
+           '			/key 1835363957' +
+           '			/showInPalette -1' +
+           '			/type (enumerated)' +
+           '			/name [ 20' +
+           '				44656c6574652047726170686963205374796c65' +
+           '			]' +
+           '			/value 3' +
+           '		}' +
+           '	}' +
+           '	/event-7 {' +
+           '		/useRulersIn1stQuadrant 0' +
+           '		/internalName (ai_plugin_symbol_palette)' +
+           '		/localizedName [ 7' +
+           '			53796d626f6c73' +
+           '		]' +
+           '		/isOpen 0' +
+           '		/isOn 1' +
+           '		/hasDialog 0' +
+           '		/parameterCount 1' +
+           '		/parameter-1 {' +
+           '			/key 1835363957' +
+           '			/showInPalette -1' +
+           '			/type (enumerated)' +
+           '			/name [ 17' +
+           '				53656c65637420416c6c20556e75736564' +
+           '			]' +
+           '			/value 12' +
+           '		}' +
+           '	}' +
+           '	/event-8 {' +
+           '		/useRulersIn1stQuadrant 0' +
+           '		/internalName (ai_plugin_symbol_palette)' +
+           '		/localizedName [ 7' +
+           '			53796d626f6c73' +
+           '		]' +
+           '		/isOpen 0' +
+           '		/isOn 1' +
+           '		/hasDialog 1' +
+           '		/showDialog 0' +
+           '		/parameterCount 1' +
+           '		/parameter-1 {' +
+           '			/key 1835363957' +
+           '			/showInPalette -1' +
+           '			/type (enumerated)' +
+           '			/name [ 13' +
+           '				44656c6574652053796d626f6c' +
+           '			]' +
+           '			/value 5' +
+           '		}' +
+           '	}' +
+           '}';
 
   }
 }());
