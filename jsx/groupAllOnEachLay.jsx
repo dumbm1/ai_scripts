57866ca371701975a4a84d58da3117acb4424661
Marat Shagiev <m_js@bk.ru>
2019-11-28 18:11:18 +0300

    add jsx groupAllOnEachLay script

---
+++ jsx/groupAllOnEachLay.jsx
@@ -0,0 +1,18 @@
+﻿//@target illustrator-22
+
+;(function groupAllOnEachLay() {
+  var doc  = activeDocument,
+      lays = doc.layers;
+
+  executeMenuCommand('unlockAll');
+  for (var i = 0; i < lays.length; i++) {lays[i].visible = false}
+
+  for (var i = 0; i < lays.length; i++) {
+    lays[i].visible = true;
+    executeMenuCommand('selectall');
+    executeMenuCommand('group');
+    lays[i].visible = false;
+  }
+
+  for (var i = 0; i < lays.length; i++) {lays[i].visible = true}
+}());
