a47d3535e72c99f43f716cc12176c680a0dbc2f2
Marat Shagiev <m_js@bk.ru>
2020-06-08 17:43:18 +0300

    change groupAllOnEachLay.jsx

--- jsx/groupAllOnEachLay.jsx
+++ jsx/groupAllOnEachLay.jsx
@@ -1,13 +1,27 @@
 ﻿//@target illustrator-22
 
+/**
+ * !!! WARNING !!!
+ * !!! IT`S VERY DANGEROUS SCRIPT !!!
+ *
+ * unlock layers
+ * unlock all items
+ * make unvisible all layers
+ * group all objects on layers exclude layers that contained sublayers
+ * make visible all layers
+ * */
+
 ;(function groupAllOnEachLay() {
   var doc  = activeDocument,
       lays = doc.layers;
 
+  for (var i = 0; i < lays.length; i++) {lays[i].visible = true}
+  for (var i = 0; i < lays.length; i++) {lays[i].locked = false}
   executeMenuCommand('unlockAll');
   for (var i = 0; i < lays.length; i++) {lays[i].visible = false}
 
   for (var i = 0; i < lays.length; i++) {
+    if (lays[i].layers.length) continue;
     lays[i].visible = true;
     executeMenuCommand('selectall');
     executeMenuCommand('group');
