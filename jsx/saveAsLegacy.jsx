f6a29496f2d3a1ec7b5c15e4882d727a4558e0e7
Marat Shagiev <m_js@bk.ru>
2020-06-11 13:02:56 +0300

    next work version of jsx/saveAsLegacy

--- jsx/saveAsLegacy.jsx
+++ jsx/saveAsLegacy.jsx
@@ -1,112 +1,124 @@
 ;(function saveAsLegacy(opts) {
+  var opts = opts || {};
   var legacyVersion = opts.legacyVersion || 'CC';
-  var setName = opts.setName || 'saveAsLegacy';
+  var setName = opts.actionSetName || 'saveAsLegacy';
   var actionName = opts.actionName || 'saveAsLegacy';
+  var isCreatePdfCompatible,
+      isIncludeLinkedFiles,
+      isEmbedICCProfiles,
+      isUseCompression,
+      isSaveEachArtboardToASeparateFile;
 
-  _runAction('saveAsCC', 'saveAsCC', _makeActStr());
+  alert([legacyVersion, setName, actionName])
 
-  function _makeActStr() {
-    return "/version 3" +
-           "/name [ " + __encodeStr2Ansii(actionName).length / 2 +
-           "	" + __encodeStr2Ansii(actionName) +
+  _runAction(actionName, setName, _getSeveAsLegasyActString());
+
+  function _getSeveAsLegasyActString() {
+
+    return "" +
+           "/version 3" +
+           "/name [ " +
+           __encodeStr2Ansii(setName).length / 2 + " " +
+           __encodeStr2Ansii(setName) +
            "]" +
            "/isOpen 1" +
            "/actionCount 1" +
            "/action-1 {" +
-           "	/name [ " + __encodeStr2Ansii(setName).length / 2
-    "		" + __encodeStr2Ansii(setName) +
-    "	]" +
-    "	/keyIndex 0" +
-    "	/colorIndex 0" +
-    "	/isOpen 1" +
-    "	/eventCount 1" +
-    "	/event-1 {" +
-    "		/useRulersIn1stQuadrant 0" +
-    "		/internalName (adobe_saveDocumentAs)" +
-    "		/localizedName [ 7" +
-    "			53617665204173" +
-    "		]" +
-    "		/isOpen 1" +
-    "		/isOn 1" +
-    "		/hasDialog 1" +
-    "		/showDialog 0" +
-    "		/parameterCount 11" +
-    "		/parameter-1 {" +
-    "			/key 1668116594" +
-    "			/showInPalette -1" +
-    "			/type (boolean)" +
-    "			/value 1" +
-    "		}" +
-    "		/parameter-2 {" +
-    "			/key 1885627936" +
-    "			/showInPalette -1" +
-    "			/type (boolean)" +
-    "			/value 0" +
-    "		}" +
-    "		/parameter-3 {" +
-    "			/key 1668445298" +
-    "			/showInPalette -1" +
-    "			/type (integer)" +
-    "			/value 17" +
-    "		}" +
-    "		/parameter-4 {" +
-    "			/key 1702392878" +
-    "			/showInPalette -1" +
-    "			/type (integer)" +
-    "			/value 1" +
-    "		}" +
-    "		/parameter-5 {" +
-    "			/key 1768842092" +
-    "			/showInPalette -1" +
-    "			/type (integer)" +
-    "			/value 0" +
-    "		}" +
-    "		/parameter-6 {" +
-    "			/key 1918989423" +
-    "			/showInPalette -1" +
-    "			/type (real)" +
-    "			/value 100.0" +
-    "		}" +
-    "		/parameter-7 {" +
-    "			/key 1886545516" +
-    "			/showInPalette -1" +
-    "			/type (integer)" +
-    "			/value 0" +
-    "		}" +
-    "		/parameter-8 {" +
-    "			/key 1936548194" +
-    "			/showInPalette -1" +
-    "			/type (boolean)" +
-    "			/value 0" +
-    "		}" +
-    "		/parameter-9 {" +
-    "			/key 1851878757" +
-    "			/showInPalette -1" +
-    "			/type (ustring)" +
-    "			/value [ " +
-    __encodeStr2Ansii((new File((activeDocument.path + '/' + activeDocument.name + '').slice(0, -3) + "_CC.ai")).fsName + '').length / 2 + " " +
-    __encodeStr2Ansii((new File((activeDocument.path + '/' + activeDocument.name + '').slice(0, -3) + "_CC.ai")).fsName + '') +
-    "			]" +
-    "		}" +
-    "		/parameter-10 {" +
-    "			/key 1718775156" +
-    "			/showInPalette -1" +
-    "			/type (ustring)" +
-    "			/value [ 35" +
-    "				41646f626520496c6c7573747261746f7220416e7920466f726d617420577269" +
-    "				746572" +
-    "			]" +
-    "		}" +
-    "		/parameter-11 {" +
-    "			/key 1702392942" +
-    "			/showInPalette -1" +
-    "			/type (ustring)" +
-    "			/value [ 6" +
-    "				61692c616974" +
-    "			]" +
-    "		}" +
-    "	}" +
-    "}";
+           "	/name [ " +
+           __encodeStr2Ansii(actionName).length / 2 + " " +
+           __encodeStr2Ansii(actionName) +
+           "	]" +
+           "	/keyIndex 0" +
+           "	/colorIndex 0" +
+           "	/isOpen 1" +
+           "	/eventCount 1" +
+           "	/event-1 {" +
+           "		/useRulersIn1stQuadrant 0" +
+           "		/internalName (adobe_saveDocumentAs)" +
+           "		/localizedName [ 7" +
+           "			53617665204173" +
+           "		]" +
+           "		/isOpen 1" +
+           "		/isOn 1" +
+           "		/hasDialog 1" +
+           "		/showDialog 0" +
+           "		/parameterCount 11" +
+           "		/parameter-1 {" +
+           "			/key 1668116594" +
+           "			/showInPalette -1" +
+           "			/type (boolean)" +
+           "			/value 1" +
+           "		}" +
+           "		/parameter-2 {" +
+           "			/key 1885627936" +
+           "			/showInPalette -1" +
+           "			/type (boolean)" +
+           "			/value 0" +
+           "		}" +
+           "		/parameter-3 {" +
+           "			/key 1668445298" +
+           "			/showInPalette -1" +
+           "			/type (integer)" +
+           "			/value 17" +
+           "		}" +
+           "		/parameter-4 {" +
+           "			/key 1702392878" +
+           "			/showInPalette -1" +
+           "			/type (integer)" +
+           "			/value 1" +
+           "		}" +
+           "		/parameter-5 {" +
+           "			/key 1768842092" +
+           "			/showInPalette -1" +
+           "			/type (integer)" +
+           "			/value 0" +
+           "		}" +
+           "		/parameter-6 {" +
+           "			/key 1918989423" +
+           "			/showInPalette -1" +
+           "			/type (real)" +
+           "			/value 100.0" +
+           "		}" +
+           "		/parameter-7 {" +
+           "			/key 1886545516" +
+           "			/showInPalette -1" +
+           "			/type (integer)" +
+           "			/value 0" +
+           "		}" +
+           "		/parameter-8 {" +
+           "			/key 1936548194" +
+           "			/showInPalette -1" +
+           "			/type (boolean)" +
+           "			/value 0" +
+           "		}" +
+           "		/parameter-9 {" +
+           "			/key 1851878757" +
+           "			/showInPalette -1" +
+           "			/type (ustring)" +
+           "			/value [ " +
+           __encodeStr2Ansii((new File((activeDocument.path + '/' + activeDocument.name + '').slice(0, -3) + "_CC.ai")).fsName + '').length / 2 + " " +
+           __encodeStr2Ansii((new File((activeDocument.path + '/' + activeDocument.name + '').slice(0, -3) + "_CC.ai")).fsName + '') +
+           "			]" +
+           "		}" +
+           "		/parameter-10 {" +
+           "			/key 1718775156" +
+           "			/showInPalette -1" +
+           "			/type (ustring)" +
+           "			/value [ 35" +
+           "				41646f626520496c6c7573747261746f7220416e7920466f726d617420577269" +
+           "				746572" +
+           "			]" +
+           "		}" +
+           "		/parameter-11 {" +
+           "			/key 1702392942" +
+           "			/showInPalette -1" +
+           "			/type (ustring)" +
+           "			/value [ 6" +
+           "				61692c616974" +
+           "			]" +
+           "		}" +
+           "	}" +
+           "}";
 
     function __encodeStr2Ansii(str) {
       var result = '';
@@ -134,7 +146,7 @@
     file.remove();
   }
 
-  function getLegacyVersionNumber(str) {
+  function _getLegacyVersionNumber(str) {
     var legacyVersionNumbers = {
       '2020': '18',
       'CC'  : '17',
