96c809557b398091aeed465211a8cf6e2162dcad
Marat Shagiev <m_js@bk.ru>
2020-06-17 23:58:06 +0300

    Make first probably stable version of fast_relink.jsx with fix relink of the rotated items

--- ord/fastRelink/fast_relink.jsx
+++ ord/fastRelink/fast_relink.jsx
@@ -5,13 +5,14 @@
  * relink all links in current document selection to one that you select in dialog
  *
  * use:
- * select raster/placed/embedded items
- * run script
- * in dialog window select new link file (and push OK)
+ * 1. select raster/placed/embedded items... or all items in document
+ * 2. run script
+ * 3. in dialog window select new link file >> OK
  *
- * result: all selected items relink on new link file
+ * result: all selected items relink to new link file
  *
- * Marat Shagiev m_js@bk.ru 17.10.2020
+ * Contact me: Marat Shagiev, m_js@bk.ru
+ * 17.10.2020
  *
  **/
 
@@ -21,40 +22,40 @@ function fast_relink() {
 
   try {
 
-    var newLinkFile = (File(activeDocument.fullName).openDlg()).fsName + '';
+    if (!documents.length) throw new Error('No document');
 
-    /*    if (selection.length == 0) {
-     throw new Error('No selected items');
-     return;
-     } else if (_getPlaced().length == 0 && _getRasters().length == 0 && _getEmbedded().length == 0) {
-     throw new Error('No raster/placed/embedded items in selection');
-     return;
-     }
+    if (selection.length == 0) {
+      throw new Error('No selected items');
+      return;
+    } else if (_getPlaced().length == 0 && _getRasters().length == 0 && _getEmbedded().length == 0) {
+      throw new Error('No raster/placed/embedded items in selection');
+      return;
+    }
 
-     var doc = activeDocument;
-     var path; // base folder that open in File(path).openDlg()
+    var doc = activeDocument;
+    var path;
 
-     if (!(File(doc.fullName).exists)) {
-     path = Folder.desktop;
-     } else {
-     path = doc.fullName;
-     }
+    if (!(File(doc.fullName).exists)) {
+      path = Folder.desktop;
+    } else {
+      path = doc.fullName;
+    }
 
-     var newLinkFile = File(path).openDlg(); // The folder where containing the active document will open
+    var newLinkFile = File(path).openDlg();
 
-     if (!newLinkFile) {
-     throw new Error('No new file to link');
-     return;
-     }*/
+    if (!newLinkFile) {
+      throw new Error('No new file to link');
+      return;
+    }
 
     var embedded = _getEmbedded();
-    // var placed = _getPlaced();
-    // var rasters = _getRasters();
+    var placed = _getPlaced();
+    var rasters = _getRasters();
 
     executeMenuCommand('deselectall');
 
-    // _relinkAllPlaced(placed, newLinkFile);
-    // _relinkAllRasters(rasters, newLinkFile);
+    _relinkAllPlaced(placed, newLinkFile);
+    _relinkAllRasters(rasters, newLinkFile);
     _relinkAllEmbedded(embedded, newLinkFile);
 
   } catch (e) {
@@ -75,20 +76,16 @@ function fast_relink() {
     }
     return rastersArr;
   }
-  function _relinkAllRasters(rasters, linkFile) {
-    for (var i = 0; i < rasters.length; i++) {
-      __relinkOneRaster(rasters[i], linkFile);
+
+  function _relinkAllRasters(rasters, newLinkFile) {
+    for (var i = rasters.length - 1; i >= 0; i--) {
+      rasters[i].selected = true;
+      __relinkOneRaster(newLinkFile);
+      executeMenuCommand('deselectall');
     }
 
-    function __relinkOneRaster(raster, linkFile) {
-      var linkItem = raster.layer.placedItems.add();
-      linkItem.file = linkFile;
-      linkItem.height = raster.height;
-      linkItem.width = raster.width;
-      linkItem.position = raster.position;
-      ___moveAfter(linkItem, raster);
-      raster.remove();
-      return linkItem;
+    function __relinkOneRaster(newLinkFile) {
+      __relinkByAction(newLinkFile);
     }
   }
 
@@ -103,14 +100,14 @@ function fast_relink() {
     }
     return placedArr;
   }
-  function _relinkAllPlaced(placed, linkFile) {
+
+  function _relinkAllPlaced(placed, newLinkFile) {
     for (var i = 0; i < placed.length; i++) {
-      __relinkOnePlaced(placed[i], linkFile);
+      __relinkOnePlaced(placed[i], newLinkFile);
     }
 
-    function __relinkOnePlaced(placed, linkFile) {
-      placed.file = linkFile;
-      return placed;
+    function __relinkOnePlaced(placed, newLinkFile) {
+      placed.file = newLinkFile;
     }
   }
 
@@ -127,6 +124,7 @@ function fast_relink() {
     }
     return embeddedArr;
   }
+
   function _relinkAllEmbedded(embedded, newLinkFile) {
     for (var i = embedded.length - 1; i >= 0; i--) {
       embedded[i].selected = true;
@@ -139,135 +137,122 @@ function fast_relink() {
     }
   }
 
-  /**
-   * move item after target
-   * @param {Object} item
-   * @param {Object} target
-   * */
-  function ___moveAfter(item, target) {
-    try {
-      if (target && item) {
-        item.move(target, ElementPlacement.PLACEAFTER);
-      }
-    } catch (e) {
-    }
-  }
-
   function __relinkByAction(newLinkFile) {
     var actName = '__fastRelink__';
+    var newLinkFileFsPath = newLinkFile.fsName + '';
 
-    _runAction(actName, actName, _getFastRelinkActString(newLinkFile, actName));
+    _runAction(actName, actName, _getFastRelinkActString(newLinkFileFsPath, actName));
 
-    function _getFastRelinkActString(newLinkFile, actName) {
+    function _getFastRelinkActString(newLinkFileFsPath, actName) {
       var actName = __encodeStr2Ansii(actName);
-      var newLinkFile = __encodeStr2Ansii(newLinkFile);
+      var newLinkFileFsPathEncoded = __encodeStr2Ansii(newLinkFileFsPath);
       var actRelinkString = "/version 3" +
-                            "/name [ " +
-                            actName.length / 2 + " " +
-                            actName +
-                            "]" +
-                            "/isOpen 1" +
-                            "/actionCount 1" +
-                            "/action-1 {" +
-                            "	/name [ " +
-                            actName.length / 2 + " " +
-                            actName +
-                            "	]" +
-                            "	/keyIndex 0" +
-                            "	/colorIndex 0" +
-                            "	/isOpen 1" +
-                            "	/eventCount 1" +
-                            "	/event-1 {" +
-                            "		/useRulersIn1stQuadrant 0" +
-                            "		/internalName (adobe_placeDocument)" +
-                            "		/localizedName [ 5" +
-                            "			506c616365" +
-                            "		]" +
-                            "		/isOpen 1" +
-                            "		/isOn 1" +
-                            "		/hasDialog 1" +
-                            "		/showDialog 0" +
-                            "		/parameterCount 12" +
-                            "		/parameter-1 {" +
-                            "			/key 1885431653" +
-                            "			/showInPalette -1" +
-                            "			/type (integer)" +
-                            "			/value 1" +
-                            "		}" +
-                            "		/parameter-2 {" +
-                            "			/key 1668444016" +
-                            "			/showInPalette -1" +
-                            "			/type (enumerated)" +
-                            "			/name [ 7" +
-                            "				43726f7020546f" +
-                            "			]" +
-                            "			/value 1" +
-                            "		}" +
-                            "		/parameter-3 {" +
-                            "			/key 1885823860" +
-                            "			/showInPalette -1" +
-                            "			/type (integer)" +
-                            "			/value 1" +
-                            "		}" +
-                            "		/parameter-4 {" +
-                            "			/key 1851878757" +
-                            "			/showInPalette -1" +
-                            "			/type (ustring)" +
-                            "			/value [ " +
-                            newLinkFile.length / 2 + " " +
-                            newLinkFile +
-                            "			]" +
-                            "		}" +
-                            "		/parameter-5 {" +
-                            "			/key 1818848875" +
-                            "			/showInPalette -1" +
-                            "			/type (boolean)" +
-                            "			/value 1" +
-                            "		}" +
-                            "		/parameter-6 {" +
-                            "			/key 1919970403" +
-                            "			/showInPalette -1" +
-                            "			/type (boolean)" +
-                            "			/value 1" +
-                            "		}" +
-                            "		/parameter-7 {" +
-                            "			/key 1953329260" +
-                            "			/showInPalette -1" +
-                            "			/type (boolean)" +
-                            "			/value 0" +
-                            "		}" +
-                            "		/parameter-8 {" +
-                            "			/key 1768779887" +
-                            "			/showInPalette -1" +
-                            "			/type (boolean)" +
-                            "			/value 0" +
-                            "		}" +
-                            "		/parameter-9 {" +
-                            "			/key 1885828462" +
-                            "			/showInPalette -1" +
-                            "			/type (boolean)" +
-                            "			/value 0" +
-                            "		}" +
-                            "		/parameter-10 {" +
-                            "			/key 1935895653" +
-                            "			/showInPalette -1" +
-                            "			/type (real)" +
-                            "			/value 1.0" +
-                            "		}" +
-                            "		/parameter-11 {" +
-                            "			/key 1953656440" +
-                            "			/showInPalette -1" +
-                            "			/type (real)" +
-                            "			/value 0.0" +
-                            "		}" +
-                            "		/parameter-12 {" +
-                            "			/key 1953656441" +
-                            "			/showInPalette -1" +
-                            "			/type (real)" +
-                            "			/value 0.0" +
-                            "		}" +
-                            "	}" +
-                            "}";
+        "/name [ " +
+        actName.length / 2 + " " +
+        actName +
+        "]" +
+        "/isOpen 0" + // in action rater2ai == 0
+        "/actionCount 1" +
+        "/action-1 {" +
+        "	/name [ " +
+        actName.length / 2 + " " +
+        actName +
+        "	]" +
+        "	/keyIndex 0" +
+        "	/colorIndex 0" +
+        "	/isOpen 1" +
+        "	/eventCount 1" +
+        "	/event-1 {" +
+        "		/useRulersIn1stQuadrant 0" +
+        "		/internalName (adobe_placeDocument)" +
+        "		/localizedName [ 5" +
+        "			506c616365" +
+        "		]" +
+        "		/isOpen 0" + // in action rater2ai == 0
+        "		/isOn 1" +
+        "		/hasDialog 1" +
+        "		/showDialog 0" +
+        "		/parameterCount 12" +
+        "		/parameter-1 {" +
+        "			/key 1885431653" +
+        "			/showInPalette -1" +
+        "			/type (integer)" +
+        "			/value 1" +
+        "		}" +
+        "		/parameter-2 {" +
+        "			/key 1668444016" +
+        "			/showInPalette -1" +
+        "			/type (enumerated)" +
+        "			/name [ 7" +
+        "				43726f7020546f" +
+        "			]" +
+        "			/value 1" +
+        "		}" +
+        "		/parameter-3 {" +
+        "			/key 1885823860" +
+        "			/showInPalette -1" +
+        "			/type (integer)" +
+        "			/value 1" +
+        "		}" +
+        "		/parameter-4 {" +
+        "			/key 1851878757" +
+        "			/showInPalette -1" +
+        "			/type (ustring)" +
+        "			/value [ " +
+        newLinkFileFsPathEncoded.length / 2 + " " +
+        newLinkFileFsPathEncoded +
+        "			]" +
+        "		}" +
+        "		/parameter-5 {" +
+        "			/key 1818848875" +
+        "			/showInPalette -1" +
+        "			/type (boolean)" +
+        "			/value 1" +
+        "		}" +
+        "		/parameter-6 {" +
+        "			/key 1919970403" +
+        "			/showInPalette -1" +
+        "			/type (boolean)" +
+        "			/value 1" +
+        "		}" +
+        "		/parameter-7 {" +
+        "			/key 1953329260" +
+        "			/showInPalette -1" +
+        "			/type (boolean)" +
+        "			/value 0" +
+        "		}" +
+        "		/parameter-8 {" +
+        "			/key 1768779887" +
+        "			/showInPalette -1" +
+        "			/type (boolean)" +
+        "			/value 0" +
+        "		}" +
+        "		/parameter-9 {" +
+        "			/key 1885828462" +
+        "			/showInPalette -1" +
+        "			/type (boolean)" +
+        "			/value 0" +
+        "		}" +
+        "		/parameter-10 {" +
+        "			/key 1935895653" +
+        "			/showInPalette -1" +
+        "			/type (real)" +
+        "			/value 1.0" +
+        "		}" +
+        "		/parameter-11 {" +
+        "			/key 1953656440" +
+        "			/showInPalette -1" +
+        "			/type (real)" +
+        "			/value 0.0" +
+        "		}" +
+        "		/parameter-12 {" +
+        "			/key 1953656441" +
+        "			/showInPalette -1" +
+        "			/type (real)" +
+        "			/value 0.0" +
+        "		}" +
+        "	}" +
+        "}";
 
       return actRelinkString;
 
@@ -299,4 +284,3 @@ function fast_relink() {
 
   }
 }
-
